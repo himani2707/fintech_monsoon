5a2a4d33345aecdf69678894581f736b5b23ae8d
Himani <Himani@Himanis-MacBook-Pro.local>
2017-05-15 22:32:22 +0530

    Change settings directory name

--- config/settings.py
+++ config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'fintech_monsoon.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'fintech_monsoon.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
